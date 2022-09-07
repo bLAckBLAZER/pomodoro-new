45196dc96a15373efa2a4db3c2ed1a818ba75263
Omkar Jadhav <jadhavomkar85@gmail.com>
2022-09-07 19:01:21 +0530

    [feat] added tests for authReducer

---
+++ src/reducers/tests/authReducer.test.ts
@@ -0,0 +1,49 @@
+import { authReducer } from "../authReducer";
+import { getDefaultAuthState } from "../../contexts/getDefaultAuthState";
+
+describe("authReducer tests", () => {
+  test("should login user", () => {
+    const expectedState = {
+      user: {
+        displayName: "Omkar",
+        email: "abc@gmail.com",
+        otherDetails: {},
+      },
+      token: "1234",
+    };
+
+    const state = authReducer(getDefaultAuthState(), {
+      type: "LOGIN",
+      payload: {
+        foundUser: {
+          displayName: "Omkar",
+          email: "abc@gmail.com",
+          otherDetails: {},
+        },
+        encodedToken: "1234",
+      },
+    });
+
+    expect(state).toEqual(expectedState);
+  });
+
+  test("should logout user", () => {
+    const expectedState = getDefaultAuthState();
+
+    const state = authReducer(
+      {
+        user: {
+          displayName: "Omkar",
+          email: "abc@gmail.com",
+          otherDetails: {},
+        },
+        token: "12345",
+      },
+      {
+        type: "LOGOUT",
+      }
+    );
+
+    expect(state).toEqual(expectedState);
+  });
+});
